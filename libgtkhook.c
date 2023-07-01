8fce6c0c04dc7b72452f4818af7495ac1f2e5893
nosajmik <nosajmik@umich.edu>
2023-07-01 00:39:17 -0400

    bunch more gtk_text_view hooks

--- libgtkhook.c
+++ libgtkhook.c
@@ -30,6 +30,20 @@ typedef void (*gtk_main_quit_func)(void);
 typedef void (*gtk_text_view_insert_func)(GtkTextView *, GtkTextBuffer *, GtkTextIter *, const gchar *, gint);
 typedef void (*gtk_text_buffer_insert_at_cursor_func)(GtkTextBuffer *, const gchar *, gint);
 typedef void (*gtk_text_buffer_insert_func)(GtkTextBuffer *, GtkTextIter *, const gchar *, gint);
+typedef void (*gtk_text_view_set_buffer_func)(GtkTextView *text_view, GtkTextBuffer *buffer);
+typedef void (*gtk_text_view_scroll_to_mark_func)(GtkTextView *text_view, GtkTextMark *mark, gdouble within_margin, gboolean use_align, gdouble xalign, gdouble yalign);
+typedef GtkTextBuffer *(*gtk_text_view_get_buffer_func)(GtkTextView *text_view);
+typedef gchar *(*gtk_text_buffer_get_text_func)(GtkTextBuffer *buffer, const GtkTextIter *start, const GtkTextIter *end, gboolean include_hidden_chars);
+typedef void (*gtk_text_buffer_delete_func)(GtkTextBuffer *buffer, GtkTextIter *start, GtkTextIter *end);
+typedef void (*gtk_text_buffer_get_start_iter_func)(GtkTextBuffer *buffer, GtkTextIter *iter);
+typedef void (*gtk_text_buffer_get_end_iter_func)(GtkTextBuffer *buffer, GtkTextIter *iter);
+typedef void (*gtk_text_view_get_visible_rect_func)(GtkTextView *text_view, GdkRectangle *visible_rect);
+typedef gboolean (*gtk_text_view_get_iter_at_position_func)(GtkTextView *text_view, GtkTextIter *iter, gint* trailing, gint x, gint y);
+typedef void (*gtk_text_view_get_iter_location_func)(GtkTextView *text_view, const GtkTextIter *iter, GdkRectangle *location);
+typedef gboolean (*gtk_text_view_get_iter_at_location_func)(GtkTextView *text_view, GtkTextIter *iter, gint x, gint y);
+typedef void (*gtk_text_view_window_to_buffer_coords_func)(GtkTextView *text_view, GtkTextWindowType win, gint window_x, gint window_y, gint *buffer_x, gint *buffer_y);
+typedef void (*gtk_text_view_buffer_to_window_coords_func)(GtkTextView *text_view, GtkTextWindowType win, gint buffer_x, gint buffer_y, gint *window_x, gint *window_y);
+typedef void (*gtk_text_view_get_line_at_y_func)(GtkTextView *text_view, GtkTextIter *target_iter, gint y, gint *line_top);
 
 void gtk_button_clicked(GtkButton *button)
 {
@@ -309,4 +323,158 @@ void gtk_text_buffer_insert(GtkTextBuffer *buffer, GtkTextIter *iter, const gcha
         (gtk_text_buffer_insert_func)dlsym(RTLD_NEXT, "gtk_text_buffer_insert");
 
     original_gtk_text_buffer_insert(buffer, iter, text, len);
+}
+
+void gtk_text_view_set_buffer(GtkTextView *text_view, GtkTextBuffer *buffer)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_view_set_buffer_func original_gtk_text_view_set_buffer =
+        (gtk_text_view_set_buffer_func)dlsym(RTLD_NEXT, "gtk_text_view_set_buffer");
+
+    original_gtk_text_view_set_buffer(text_view, buffer);
+}
+
+void gtk_text_view_scroll_to_mark(GtkTextView *text_view, GtkTextMark *mark, gdouble within_margin, gboolean use_align, gdouble xalign, gdouble yalign)
+{
+    static uint64_t counter = 0;
+    printf("%s: within_margin=%f, use_align=%d, xalign=%f, yalign=%f: %lu\n", __func__, within_margin, use_align, xalign, yalign, counter++);
+
+    gtk_text_view_scroll_to_mark_func original_gtk_text_view_scroll_to_mark =
+        (gtk_text_view_scroll_to_mark_func)dlsym(RTLD_NEXT, "gtk_text_view_scroll_to_mark");
+
+    original_gtk_text_view_scroll_to_mark(text_view, mark, within_margin, use_align, xalign, yalign);
+}
+
+GtkTextBuffer *gtk_text_view_get_buffer(GtkTextView *text_view)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_view_get_buffer_func original_gtk_text_view_get_buffer =
+        (gtk_text_view_get_buffer_func)dlsym(RTLD_NEXT, "gtk_text_view_get_buffer");
+
+    return original_gtk_text_view_get_buffer(text_view);
+}
+
+gchar *gtk_text_buffer_get_text(GtkTextBuffer *buffer, const GtkTextIter *start, const GtkTextIter *end, gboolean include_hidden_chars)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_buffer_get_text_func original_gtk_text_buffer_get_text =
+        (gtk_text_buffer_get_text_func)dlsym(RTLD_NEXT, "gtk_text_buffer_get_text");
+
+    return original_gtk_text_buffer_get_text(buffer, start, end, include_hidden_chars);
+}
+
+void gtk_text_buffer_delete(GtkTextBuffer *buffer, GtkTextIter *start, GtkTextIter *end)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_buffer_delete_func original_gtk_text_buffer_delete =
+        (gtk_text_buffer_delete_func)dlsym(RTLD_NEXT, "gtk_text_buffer_delete");
+
+    original_gtk_text_buffer_delete(buffer, start, end);
+}
+
+void gtk_text_buffer_get_start_iter(GtkTextBuffer *buffer, GtkTextIter *iter)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_buffer_get_start_iter_func original_gtk_text_buffer_get_start_iter =
+        (gtk_text_buffer_get_start_iter_func)dlsym(RTLD_NEXT, "gtk_text_buffer_get_start_iter");
+
+    original_gtk_text_buffer_get_start_iter(buffer, iter);
+}
+
+void gtk_text_buffer_get_end_iter(GtkTextBuffer *buffer, GtkTextIter *iter)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_buffer_get_end_iter_func original_gtk_text_buffer_get_end_iter =
+        (gtk_text_buffer_get_end_iter_func)dlsym(RTLD_NEXT, "gtk_text_buffer_get_end_iter");
+
+    original_gtk_text_buffer_get_end_iter(buffer, iter);
+}
+
+void gtk_text_view_get_visible_rect(GtkTextView *text_view, GdkRectangle *visible_rect)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_view_get_visible_rect_func original_func =
+        (gtk_text_view_get_visible_rect_func)dlsym(RTLD_NEXT, "gtk_text_view_get_visible_rect");
+
+    original_func(text_view, visible_rect);
+}
+
+gboolean gtk_text_view_get_iter_at_position(GtkTextView *text_view, GtkTextIter *iter, gint *trailing, gint x, gint y)
+{
+    static uint64_t counter = 0;
+    printf("%s: x=%d, y=%d: %lu\n", __func__, x, y, counter++);
+
+    gtk_text_view_get_iter_at_position_func original_func =
+        (gtk_text_view_get_iter_at_position_func)dlsym(RTLD_NEXT, "gtk_text_view_get_iter_at_position");
+
+    return original_func(text_view, iter, trailing, x, y);
+}
+
+void gtk_text_view_get_iter_location(GtkTextView *text_view, const GtkTextIter *iter, GdkRectangle *location)
+{
+    static uint64_t counter = 0;
+    printf("%s: %lu\n", __func__, counter++);
+
+    gtk_text_view_get_iter_location_func original_func =
+        (gtk_text_view_get_iter_location_func)dlsym(RTLD_NEXT, "gtk_text_view_get_iter_location");
+
+    original_func(text_view, iter, location);
+}
+
+gboolean gtk_text_view_get_iter_at_location(GtkTextView *text_view, GtkTextIter *iter, gint x, gint y)
+{
+    static uint64_t counter = 0;
+    printf("%s: x=%d, y=%d: %lu\n", __func__, x, y, counter++);
+
+    gtk_text_view_get_iter_at_location_func original_func =
+        (gtk_text_view_get_iter_at_location_func)dlsym(RTLD_NEXT, "gtk_text_view_get_iter_at_location");
+
+    return original_func(text_view, iter, x, y);
+}
+
+void gtk_text_view_window_to_buffer_coords(GtkTextView *text_view, GtkTextWindowType win, gint window_x, gint window_y, gint *buffer_x, gint *buffer_y)
+{
+    static uint64_t counter = 0;
+    printf("%s: window_x=%d, window_y=%d: %lu\n", __func__, window_x, window_y, counter++);
+
+    gtk_text_view_window_to_buffer_coords_func original_func =
+        (gtk_text_view_window_to_buffer_coords_func)dlsym(RTLD_NEXT, "gtk_text_view_window_to_buffer_coords");
+
+    original_func(text_view, win, window_x, window_y, buffer_x, buffer_y);
+}
+
+void gtk_text_view_buffer_to_window_coords(GtkTextView *text_view, GtkTextWindowType win, gint buffer_x, gint buffer_y, gint *window_x, gint *window_y)
+{
+    static uint64_t counter = 0;
+    printf("%s: buffer_x=%d, buffer_y=%d: %lu\n", __func__, buffer_x, buffer_y, counter++);
+
+    gtk_text_view_buffer_to_window_coords_func original_func =
+        (gtk_text_view_buffer_to_window_coords_func)dlsym(RTLD_NEXT, "gtk_text_view_buffer_to_window_coords");
+
+    original_func(text_view, win, buffer_x, buffer_y, window_x, window_y);
+}
+
+void gtk_text_view_get_line_at_y(GtkTextView *text_view, GtkTextIter *target_iter, gint y, gint *line_top)
+{
+    static uint64_t counter = 0;
+    printf("%s: y=%d: %lu\n", __func__, y, counter++);
+
+    gtk_text_view_get_line_at_y_func original_func =
+        (gtk_text_view_get_line_at_y_func)dlsym(RTLD_NEXT, "gtk_text_view_get_line_at_y");
+
+    return original_func(text_view, target_iter, y, line_top);
 }
